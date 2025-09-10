14bc886b165b3002a1a35be7bd9d172061abe453
Prince Yadav <py7496813@gmail.com>
2025-09-10 10:36:57 +0530

    Doo while loop

---
+++ Loop/DoWhile.java
@@ -0,0 +1,11 @@
+// when i = 2 then print number 2 to 20 till.
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i=25;     // when while condition false then print value of i. 
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=20);
+    }
+}
