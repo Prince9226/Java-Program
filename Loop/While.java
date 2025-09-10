9843356e29f4e5e068f68b00efeab2c451b32e1e
Prince Yadav <py7496813@gmail.com>
2025-09-10 10:27:46 +0530

    while loop

---
+++ Loop/While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+        int n=0;
+        while(n<=20){
+            System.out.println(n);
+            n++;
+        }
+
+    }
+    
+}
