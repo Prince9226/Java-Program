5aa37d02e6a6f54da3ea595ecd942e822190f6c9
Prince Yadav <py7496813@gmail.com>
2025-09-13 01:19:01 +0530

    Array define

---
+++ Array/Arraysfirst.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Arraysfirst {
+    public static void main(String[] args) {
+        /*int marks[]=new int[3];
+        marks[0]=23;
+        marks[1]=33;
+        marks[2]=65;*/
+
+        int marks[]={23,33,65};    // other way for array initilize
+
+        for(int i = 0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
