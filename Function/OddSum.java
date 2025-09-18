afff76cb7209f5d5d2f15713ed5ab21105af4d70
Prince Yadav <py7496813@gmail.com>
2025-09-18 21:49:11 +0530

    Sum of all odd number

--- Function/OddSum.java
+++ Function/OddSum.java
@@ -1,7 +1,27 @@
 // Write a function to print the sum of all odd numbers from 1 to n.
 
 import java.util.*;
+
 public class OddSum {
-    
-    
+    public static int countodd(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int totalSum = countodd(num);
+
+        // output
+
+        System.out.println("The sum of all odd numbers up to " + num + " is: " + totalSum);
+    }
+
 }
